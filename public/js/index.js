d0308f97bbd231c7b31aa531b323a9de10222c92
marinkovik <marinkovik.slobodan@gmail.com>
2018-07-03 21:04:38 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Cobe',
-    text: 'Yup, thats me'
-  });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=> {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'John',
-    text: 'See you again',
-    createAt: 123
-  });
-
   socket.on('createMessage', (message) =>{
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.form,
+      text: message.text,
+      createAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', ()=>{
